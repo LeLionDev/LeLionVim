4dc01ce86dfd89ecbbb0172a340b4feb6f8815f0
LeLionDev <140218623+LeLionDev@users.noreply.github.com>
2025-05-28 20:24:00 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
+# donwload dependencies for manual build of neovim https://github.com/neovim/neovim/blob/master/BUILD.md
+sudo apt install make
+sudo apt install cmake
+
+mkdir ~/applications
+cd ~/applications
+
+git clone https://github.com/neovim/neovim
+git checkout release-0.11 # latest avaialble 2025
+
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+
+sudo make install
+
 # get config from git
 mkdir ~/cfg
 cd ~/cfg
